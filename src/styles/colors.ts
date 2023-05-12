ce2880ae3a41bf09dd842443692907f7be8bea71
˜cesarvargasjr˜ <cesar_vargas_jr@hotmail.com>
2023-05-12 16:23:52 -0300

    Settings inital

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  black: '#000',
+  white: '#FFF',
+  red: '#F5222D',
+  green: '#00A676',
+  orange: '#FF6424',
+  greyLight: '#F3F2F2',
+  greySecondary: '#29292e',
+  grey: '#5a5a5a',
+};
